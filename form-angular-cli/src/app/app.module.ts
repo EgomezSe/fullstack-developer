10ac5b58003a370978d2b29b2b2e8b836f9c0647
Edwar Fernando Gómez Serna <edwarg78@gmail.com>
2020-08-16 08:44:49 -0500

    Se realiza conexión con servicios

--- form-angular-cli/src/app/app.module.ts
+++ form-angular-cli/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule, MatCardModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatInputModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
